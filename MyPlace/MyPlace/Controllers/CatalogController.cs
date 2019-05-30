8f05e442502be726fbebc62028ddc11f91e34998
Anastas Kosstow <anastas.kosstow@protonmail.com>
2019-05-30 15:11:40 +0300

    add caching

--- MyPlace/MyPlace/Controllers/CatalogController.cs
+++ MyPlace/MyPlace/Controllers/CatalogController.cs
@@ -2,20 +2,27 @@
 namespace MyPlace.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Caching.Memory;
     using MyPlace.Models;
     using MyPlace.Models.Catalog;
     using MyPlace.Services.Contracts;
 
     public class CatalogController : Controller
     {
+        private readonly IMemoryCache _cache;
         private readonly ICatalogService _catalogService;
 
-        public CatalogController(ICatalogService catalogContex) =>
-            _catalogService = catalogContex ?? throw new ArgumentNullException(nameof(catalogContex));
+        public CatalogController(ICatalogService catalogContex, IMemoryCache cache)
+        {
+            _cache = cache;
+            _catalogService = catalogContex;
+        }
+            
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() =>
@@ -42,10 +49,22 @@ namespace MyPlace.Controllers
         public async Task<IActionResult> Establishment(int Id) =>
             View(await _catalogService.GetByIdAsync<EstablishmentIndexModel>(Id));
 
-        public IActionResult Demo() => View();
 
-        public JsonResult GetAll() =>
-             Json(_catalogService.AutocompleteGetAll());
+
+        public JsonResult GetAll()
+        {
+            IEnumerable<string> cacheEntry;
+
+            if (!_cache.TryGetValue("AutocompleteValues", out cacheEntry))
+            {
+                cacheEntry = _catalogService.AutocompleteGetAll();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(1));
+                _cache.Set("AutocompleteValues", cacheEntry, cacheEntryOptions);
+            }
+            return Json(cacheEntry);
+        }
     }
 }
 
