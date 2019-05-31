0509f5bd6d511f4ddbd16982f48b5050d941f192
Anastas Kosstow <anastas.kosstow@protonmail.com>
2019-06-01 00:17:46 +0300

    Fix: comments, Add: Character count for comment

--- MyPlace/MyPlace/wwwroot/js/site.js
+++ MyPlace/MyPlace/wwwroot/js/site.js
@@ -17,3 +17,18 @@ $(document).ready(function () {
 });
 
 
+var text_max = 200;
+$('#count_message').html(text_max + ' remaining');
+
+$('#comment-area').keyup(function () {
+    var text_val = $('#comment-area').val();
+    var text_length = $('#comment-area').val().length;
+    if (text_length > text_max) {
+        $('#comment-area').val((text_val).substring(0, text_length - 1));
+        return false;
+    }
+
+    var text_remaining = text_max - text_length;
+
+    $('#count_message').html(text_remaining + ' remaining');
+});
