cbb9c5b7eaf7af56b4522a3635bbf919869c6d87
veselamanolova <veselamanolov@abv.bg>
2019-11-30 23:02:06 +0200

    change notes status on click with JQuery - frontend only

---
+++ MyPlace/MyPlace/wwwroot/js/notes.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    $(document.body).on('click', '.isCompleted', function () { updateStatusToInProgress(event, this) });
+    $(document.body).on('click', '.isInProgress', function () { updateStatusToCompleted(event, this) });   
+});
+
+
+function updateStatusToInProgress(event, element) {
+    console.log("in is completed tag ");
+    $(element)
+        .replaceWith("<img src='/images/notes/in-progress.png' class='status-icon isInProgress' alt='In progress'>"); 
+}
+
+function updateStatusToCompleted(event, element) {
+    console.log("in is progress tag ");
+    $(element)
+        .replaceWith("<img src='/images/notes/completed.svg' class='status-icon isCompleted' alt='Completed'>");
+}
